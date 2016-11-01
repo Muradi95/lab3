15afca12111d94c5b875f56dfae41f751afe9640
Muradi95 <morad-199@hotmail.com>
2016-11-01 13:54:31 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,27 +5,25 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException{
-
-        System.out.println(args[0]);
         Scanner sc = new Scanner(new File(args[0]));
-        ArrayList<Integer> a = new ArrayList<Integer>();
-
-        ArrayList<Double> b = new ArrayList<Double>();
-
+        double bpm = Double.parseDouble(args[1]);
+        System.out.print(bpm);
+        int a = 0;
+        double b = 0;
         SoundDevice device = new SoundDevice();
         Song song = new Song(50);
-        int i = 0;
+
+
         while(sc.hasNext()){
             if(sc.hasNextInt())
-                a.add(sc.nextInt());
+                a = sc.nextInt();
             if(sc.hasNextDouble())
-                b.add(sc.nextDouble());
-            song.add(MusicUtils.note(a.get(i),b.get(i)));
-            i++;
+                b = sc.nextDouble();
+
+
+            song.add(MusicUtils.harmonic(a,(b*bpm)/60));
+
         }
-        song.add(MusicUtils.note(10,2));
-        song.add(MusicUtils.harmonic(2,2));
-        song.add(MusicUtils.average((MusicUtils.note(10,2)),(MusicUtils.note(2,2))));
         song.play(device);
         song.save(device.getFormat(),new File("komp1.wav"));
     }//main
